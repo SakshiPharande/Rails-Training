3888054e32e3af2b1765737fb4066bcbb4422870
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-01-21 21:50:30 +0530

    add category module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_21_113459) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_21_161500) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
